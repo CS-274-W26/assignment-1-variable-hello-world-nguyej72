7af7d84731a3875079dd10aa8a0ae7751096285f
Justin Nguyen <nguyej72@flip1.engr.oregonstate.edu>
2026-01-19 13:37:07 -0800

    Last Commit

--- prog1.c
+++ prog1.c
@@ -1,6 +1,6 @@
 #include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
+#include <stdlib.h>
+#include <time.h>
 
 
 int main() {
@@ -8,6 +8,22 @@ int main() {
 	// is executed, it will automatically start here.
 	
 	// TODO Complete the program
+	srand(time(NULL));
 
-	//hellooo
+	int myrand = rand() % 11;
+
+	if (myrand >= 0 && myrand <= 4) {
+		printf("Eat more beef, kick less cats\n");
+	} else if(myrand > 4 && myrand <= 9) {
+		printf("FRODO LIVES\n");
+	} else if(myrand == 10) {
+		printf("Karn is the best roguelike\n");
+	} else{
+		return 0;
+	}
+
+	printf("The random number was: %d\n", myrand);
+
+	return 0;
 }
+
